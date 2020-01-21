6a61a4d83709b130f7e8f7a892a6f9e2d4e67170
Daniel Stephenson <daniel@giffgaff.co.uk>
2020-01-21 12:16:06 +0000

    Signup screen & spacer style component

--- src/screens/SignupScreen.jsx
+++ src/screens/SignupScreen.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { Text, Input, Button } from 'react-native-elements';
+import Spacer from '../components/spacer';
 
-const SignupScreen = () => {
+const SignupScreen = ({ navigation }) => {
   return (
-    <View>
-      <Text>Signup Screen</Text>
-    </View>
+    <>
+      <Spacer>
+        <Text h3>Sign up for Tracker</Text>
+      </Spacer>
+      <Input label="Email" />
+      <Spacer />
+      <Input label="Password" />
+      <Spacer>
+        <Button title="Sign Up" />
+      </Spacer>
+    </>
   );
 };
 
