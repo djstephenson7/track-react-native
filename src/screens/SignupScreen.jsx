a078880f47b3857d29293cbeafa189530e0adc65
Daniel Stephenson <daniel@giffgaff.co.uk>
2020-01-22 09:32:26 +0000

    Error handling with signing up

--- src/screens/SignupScreen.jsx
+++ src/screens/SignupScreen.jsx
@@ -1,21 +1,61 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import { Text, Input, Button } from 'react-native-elements';
+import { StyleSheet, View } from 'react-native';
+import { Context as AuthContext } from '../context/authContext';
 import Spacer from '../components/spacer';
 
 const SignupScreen = ({ navigation }) => {
+  const { state, signup } = useContext(AuthContext);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
-    <>
+    <View style={styles.container}>
       <Spacer>
         <Text h3>Sign up for Tracker</Text>
       </Spacer>
-      <Input label="Email" />
+      <Input
+        autoCapitalize="none"
+        autoCorrect={false}
+        label="Email"
+        value={email}
+        onChangeText={setEmail}
+      />
       <Spacer />
-      <Input label="Password" />
+      <Input
+        secureTextEntry
+        autoCapitalize="none"
+        autoCorrect={false}
+        label="Password"
+        value={password}
+        onChangeText={setPassword}
+      />
+      {state.errorMessage ? (
+        <Text style={styles.error}>{state.errorMessage}</Text>
+      ) : null}
       <Spacer>
-        <Button title="Sign Up" />
+        <Button title="Sign Up" onPress={() => signup({ email, password })} />
       </Spacer>
-    </>
+    </View>
   );
 };
+
+SignupScreen.navigationOptions = () => {
+  return { header: null };
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom: 250
+  },
+  error: {
+    fontSize: 16,
+    color: 'red',
+    marginLeft: 15,
+    marginTop: 15
+  }
+});
 
 export default SignupScreen;
