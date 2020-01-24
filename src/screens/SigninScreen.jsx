da4b9a2c50944ec3c0a04d991bcfe4c9ac9c294a
Daniel Stephenson <daniel@giffgaff.co.uk>
2020-01-24 16:56:19 +0000

    Functioning Signin/Signup screens

--- src/screens/SigninScreen.jsx
+++ src/screens/SigninScreen.jsx
@@ -1,12 +1,35 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useContext } from 'react';
+import { View, StyleSheet } from 'react-native';
+import AuthForm from '../components/AuthForm';
+import NavLink from '../components/NavLink';
+import { Context as AuthContext } from '../context/authContext';
 
 const SigninScreen = () => {
+  const { state, signin } = useContext(AuthContext);
+
   return (
-    <View>
-      <Text>Signin Screen</Text>
+    <View style={styles.container}>
+      <AuthForm
+        headerText="Sign in"
+        errorMessage={state.errorMessage}
+        onSubmit={signin}
+        submitButtonText="Sign in"
+      />
+      <NavLink routeName="Signup" text="Don't have an account? Sign up." />
     </View>
   );
 };
+
+SigninScreen.navigationOptions = () => {
+  return { header: null };
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom: 250
+  }
+});
 
 export default SigninScreen;
