3f72468c55b99a48524b315c20e4d08f0b035bcc
Daniel Stephenson <daniel@giffgaff.co.uk>
2020-02-20 10:10:38 +0000

    Create Track component

--- src/screens/TrackCreateScreen.jsx
+++ src/screens/TrackCreateScreen.jsx
@@ -2,21 +2,21 @@ import React, { useContext } from 'react';
 import { Text } from 'react-native-elements';
 import { SafeAreaView, withNavigationFocus } from 'react-navigation';
 import Map from '../components/Map';
+import TrackForm from '../components/TrackForm';
 import { Context as LocationContext } from '../context/locationContext';
 import useLocation from '../hooks/useLocation';
 import '../_mockLocation';
 
 const TrackCreateScreen = ({ isFocused }) => {
   const { addLocation } = useContext(LocationContext);
-
   const [error] = useLocation(isFocused, addLocation);
 
   return (
     <SafeAreaView>
-      <Text h2>Track Create Screen</Text>
+      <Text h2>Create a Track</Text>
       <Map />
-
       {error ? <Text>Please enable location services.</Text> : null}
+      <TrackForm />
     </SafeAreaView>
   );
 };
